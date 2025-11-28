72706eabdfee3dc6e81cde059ffe5ee76ab136a6
ICamioneta <88626648+ICamioneta@users.noreply.github.com>
2025-11-28 20:33:32 +0100

    Basic website

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,16 @@
+﻿document.addEventListener('DOMContentLoaded', // Document Object Model
+    () =>
+{
+    // fetchBtn and message line up with HTML DOM objects
+    const fetchBtn = document.getElementById('fetchBtn');
+    const message = document.getElementById('message');
+
+    fetchBtn.addEventListener('click',
+        () =>
+    {
+        fetch('/api/hello')
+            .then(response => response.text())
+            .then(data => {message.textContent = data})
+            .catch(err => console.error('Error fetching: ', err));
+    });
+});
